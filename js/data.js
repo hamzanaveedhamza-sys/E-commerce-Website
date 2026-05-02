3102a64703831c36cb8deed2f12bfe51da3c1465
hamzanaveedhamza-sys <hamzanaveedhamza@gmail.com>
2026-05-03 00:02:38 +0500

    Add complete product data for all NAUR fragrances

---
+++ js/data.js
@@ -0,0 +1,94 @@
+const products = [
+    {
+        id: 1,
+        name: 'NAUR Fragrance 1',
+        description: 'Description of NAUR Fragrance 1',
+        price: 50,
+        imageUrl: 'url_to_image_1',
+        notes: ['note1', 'note2'],
+        category: 'fragrance',
+        sku: 'NAUR001'
+    },
+    {
+        id: 2,
+        name: 'NAUR Fragrance 2',
+        description: 'Description of NAUR Fragrance 2',
+        price: 55,
+        imageUrl: 'url_to_image_2',
+        notes: ['note3', 'note4'],
+        category: 'fragrance',
+        sku: 'NAUR002'
+    },
+    {
+        id: 3,
+        name: 'NAUR Fragrance 3',
+        description: 'Description of NAUR Fragrance 3',
+        price: 60,
+        imageUrl: 'url_to_image_3',
+        notes: ['note5', 'note6'],
+        category: 'fragrance',
+        sku: 'NAUR003'
+    },
+    {
+        id: 4,
+        name: 'NAUR Fragrance 4',
+        description: 'Description of NAUR Fragrance 4',
+        price: 65,
+        imageUrl: 'url_to_image_4',
+        notes: ['note7', 'note8'],
+        category: 'fragrance',
+        sku: 'NAUR004'
+    },
+    {
+        id: 5,
+        name: 'NAUR Fragrance 5',
+        description: 'Description of NAUR Fragrance 5',
+        price: 70,
+        imageUrl: 'url_to_image_5',
+        notes: ['note9', 'note10'],
+        category: 'fragrance',
+        sku: 'NAUR005'
+    },
+    {
+        id: 6,
+        name: 'NAUR Fragrance 6',
+        description: 'Description of NAUR Fragrance 6',
+        price: 75,
+        imageUrl: 'url_to_image_6',
+        notes: ['note11', 'note12'],
+        category: 'fragrance',
+        sku: 'NAUR006'
+    },
+    {
+        id: 7,
+        name: 'NAUR Fragrance 7',
+        description: 'Description of NAUR Fragrance 7',
+        price: 80,
+        imageUrl: 'url_to_image_7',
+        notes: ['note13', 'note14'],
+        category: 'fragrance',
+        sku: 'NAUR007'
+    },
+    {
+        id: 8,
+        name: 'NAUR Fragrance 8',
+        description: 'Description of NAUR Fragrance 8',
+        price: 85,
+        imageUrl: 'url_to_image_8',
+        notes: ['note15', 'note16'],
+        category: 'fragrance',
+        sku: 'NAUR008'
+    },
+    {
+        id: 9,
+        name: 'NAUR Fragrance 9',
+        description: 'Description of NAUR Fragrance 9',
+        price: 90,
+        imageUrl: 'url_to_image_9',
+        notes: ['note17', 'note18'],
+        category: 'fragrance',
+        sku: 'NAUR009'
+    }
+];
+
+export default products;
